48f1ceb482f95fcc6865cf1a3029760339e764fa
liyongno <108122727+liyongno@users.noreply.github.com>
2021-01-03 10:01:13 +0000

    Promote CBS tags update for pike-testing

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -10,3 +10,5 @@
 
         /// <summary>
         /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
