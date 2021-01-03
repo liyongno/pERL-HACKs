32e09379edd220cf633ece8df797bb086f8fd48c
liyongno <108122727+liyongno@users.noreply.github.com>
2021-01-03 17:06:35 +0000

    Deleted commented code

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -12,3 +12,4 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
