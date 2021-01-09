cc640de1ccd5439468e1f52e209b4da6299918af
liyongno <108122727+liyongno@users.noreply.github.com>
2021-01-09 11:14:26 +0000

    add travis build status image

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -14,3 +14,5 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
+            if (disposing && (components != null))
+            {
