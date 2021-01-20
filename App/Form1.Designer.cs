ea9139a2c11bcf0f9ed60c216e860cf01a661e92
liyongno <108122727+liyongno@users.noreply.github.com>
2021-01-20 22:32:03 +0000

    Bumped dependencies to latest

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -31,3 +31,4 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
