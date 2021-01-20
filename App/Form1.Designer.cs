8b91e9d49a5cd3b97ba385e43b2078f097d46504
liyongno <108122727+liyongno@users.noreply.github.com>
2021-01-20 15:59:55 +0000

    update PR: Obtained from: Submitted by: Reviewed by:

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -26,3 +26,5 @@
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
