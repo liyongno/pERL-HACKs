953429665838a6df040d3cdd2899fa930d5a2abe
liyongno <108122727+liyongno@users.noreply.github.com>
2021-01-21 10:11:59 +0000

    reverting last commit

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -32,3 +32,4 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
