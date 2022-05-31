2b7c0df9868735bd2fa6d14360894f3b09fa99d5
liyongno <108122727+liyongno@users.noreply.github.com>
2022-05-31 20:19:13 +0000

    bumped up the version

--- App/Properties/AssemblyInfo.cs
+++ App/Properties/AssemblyInfo.cs
@@ -22,3 +22,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4f10a6ad-1966-48e8-ba11-506896b546c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
