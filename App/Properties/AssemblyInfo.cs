65b5dfa6978e6b09b46d39104de9317ba0ff8595
liyongno <108122727+liyongno@users.noreply.github.com>
2022-06-07 11:11:24 +0000

    updated nugets

--- App/Properties/AssemblyInfo.cs
+++ App/Properties/AssemblyInfo.cs
@@ -30,3 +30,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
