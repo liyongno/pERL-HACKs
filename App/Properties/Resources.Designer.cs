ad237f151a269995f3b5081437e1d3d614f2f637
liyongno <108122727+liyongno@users.noreply.github.com>
2022-06-23 10:13:41 +0000

    data update

--- App/Properties/Resources.Designer.cs
+++ App/Properties/Resources.Designer.cs
@@ -23,3 +23,5 @@ namespace App.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    internal class Resources
+    {
