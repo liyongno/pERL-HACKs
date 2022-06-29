a2878165f0feb7b8ddcdc1adf7d87af551c6b9e4
liyongno <108122727+liyongno@users.noreply.github.com>
2022-06-29 09:24:11 +0000

    documentation update

--- App/Properties/Resources.Designer.cs
+++ App/Properties/Resources.Designer.cs
@@ -31,3 +31,4 @@ namespace App.Properties
         private static global::System.Globalization.CultureInfo resourceCulture;
 
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources()
