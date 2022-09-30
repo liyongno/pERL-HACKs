38cc560dcfa56c31fe7e1f7660f372d724f3e300
liyongno <108122727+liyongno@users.noreply.github.com>
2022-09-30 13:11:56 +0000

    New translations en.json (Serbian (Cyrillic))

--- App/Properties/Settings.Designer.cs
+++ App/Properties/Settings.Designer.cs
@@ -7,3 +7,7 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
+//------------------------------------------------------------------------------
+
+namespace App.Properties
+{
